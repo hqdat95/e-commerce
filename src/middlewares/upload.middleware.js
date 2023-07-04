3bbf7fcc28de5bd5df556a582800613729a96460
hqdat95 <hqdat.95vn@gmail.com>
2023-07-04 17:36:29 +0700

    Product Images API

---
+++ src/middlewares/upload.middleware.js
@@ -0,0 +1,21 @@
+import multer from 'multer';
+
+const uploadAny = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error('Only .png, .jpg and .jpeg format allowed!'), false);
+    }
+  },
+}).any();
+
+export default uploadAny;
